e779a518447deb284fe9007c96cebdf269705073
Abhimanyu Kapur <abhi.kapur09@gmail.com>
2019-04-23 22:13:22 -0400

    Done

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.881504923f36837713522351f28b7458.js"
+  "/precache-manifest.fb47cad997308cd8777c13f95c27a875.js"
 );
 
 workbox.clientsClaim();
